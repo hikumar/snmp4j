acb3d8dc96a6de1fe7be5455fccb365325704a88
Andreas Falk <mail@andreasfalk.se>
2014-05-20 10:59:30 +0200

    Remove synchronous methods from org.snmp4j.Session

--- src/main/java/org/snmp4j/Session.java
+++ src/main/java/org/snmp4j/Session.java
@@ -30,7 +30,6 @@ import java.io.IOException;
  * @version 1.2
  */
 public interface Session {
-
   /**
    * Closes the session and frees any allocated resources, i.e. sockets.
    * After a <code>Session</code> has been closed it must
@@ -39,26 +38,6 @@ public interface Session {
    *    if the session could not free all resources.
    */
   public void close() throws IOException;
-
-  /**
-   * Sends a <code>PDU</code> to the given target and returns the received
-   * response <code>PDU</code>.
-   * @param pdu
-   *    the <code>PDU</code> to send.
-   * @param target
-   *    the <code>Target</code> instance that specifies how and where to send
-   *    the PDU.
-   * @return
-   *    the received response encapsulated in a <code>ResponseEvent</code>
-   *    instance. To obtain the received response <code>PDU</code> call
-   *    {@link ResponseEvent#getResponse()}. If the request timed out,
-   *    that method will return <code>null</code>. If the sent <code>pdu</code>
-   *    is an unconfirmed PDU (notification, response, or report), then
-   *    <code>null</code> will be returned.
-   * @throws IOException
-   *    if the message could not be send.
-   */
-  public ResponseEvent send(PDU pdu, Target target) throws IOException;
 
   /**
    * Asynchronously sends a <code>PDU</code> to the given target. The response
@@ -82,39 +61,6 @@ public interface Session {
    */
   public void send(PDU pdu, Target target, Object userHandle,
                    ResponseListener listener) throws IOException;
-
-  /**
-   * Sends a <code>PDU</code> to the given target and returns the received
-   * response <code>PDU</code> encapsulated in a <code>ResponseEvent</code>
-   * object that also includes:
-   * <ul>
-   * <li>the transport address of the response sending peer,
-   * <li>the <code>Target</code> information of the target,
-   * <li>the request <code>PDU</code>,
-   * <li>the response <code>PDU</code> (if any).
-   * </ul>
-   * @param pdu
-   *    the PDU instance to send.
-   * @param target
-   *    the Target instance representing the target SNMP engine where to send
-   *    the <code>pdu</code>.
-   * @param transport
-   *    specifies the <code>TransportMapping</code> to be used when sending
-   *    the PDU. If <code>transport</code> is <code>null</code>, the associated
-   *    message dispatcher will try to determine the transport mapping by the
-   *    <code>target</code>'s address.
-   * @return
-   *    the received response encapsulated in a <code>ResponseEvent</code>
-   *    instance. To obtain the received response <code>PDU</code> call
-   *    {@link ResponseEvent#getResponse()}. If the request timed out,
-   *    that method will return <code>null</code>. If the sent <code>pdu</code>
-   *    is an unconfirmed PDU (notification, response, or report), then
-   *    <code>null</code> will be returned.
-   * @throws IOException
-   *    if the message could not be send.
-   */
-  public ResponseEvent send(PDU pdu, Target target,
-                            TransportMapping transport) throws IOException;
 
   /**
    * Asynchronously sends a <code>PDU</code> to the given target. The response
